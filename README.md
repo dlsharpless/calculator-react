c6f69c229b04cb01cb79cf7a03e2b32a0fd6db58
Thor Sharpless <10237785+dlsharpless@users.noreply.github.com>
2023-10-05 01:26:03 -0600

    Revert "Formatted with Prettier"
    This reverts commit ee73174edd918bb3bfd6dd4815b8662ebca70618.

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # calculator-react
-
 Based on "The Perfect Beginner React Project" by Web Dev Simplified
 https://youtu.be/DgRrrOt0Vr8
--- src/OperationButton.jsx
+++ src/OperationButton.jsx
@@ -1,13 +1,11 @@
-import { ACTIONS } from "./App";
+import { ACTIONS } from './App'
 
 export default function OperationButton({ dispatch, operation }) {
-  return (
-    <button
-      onClick={() =>
-        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
-      }
-    >
-      {operation}
-    </button>
-  );
+    return (
+        <button
+            onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
+        >
+            {operation}
+        </button>
+    )
 }
